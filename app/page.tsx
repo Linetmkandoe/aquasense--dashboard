564d1b7bdaf7a102440a615611e30efe00f9f153
Linet Ndau <mkandoelinet@gmail.com>
2024-10-02 17:30:46 +0300

    add file

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./dashboard/page";
+import Landinpage from "./landingpage/page";
+import Layout from "./components/Layout";
+
+export default function Home() {
+  return (
+    <div>
+      <Landinpage />
+     
+    </div>
+  );
+}
